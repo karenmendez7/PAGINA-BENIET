7553f6976763bac4bf6c2f01f8ed9603bbe2298c
root <root@ip-10-250-17-24.ec2.internal>
2022-06-15 01:26:42 +0000

    mi primer commit

---
+++ behavior.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(evento) {
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+        alerta ("¡Te vi hacer clic!");
+      });
+      var thumbnailElement = document.getElementById("smart_thumbnail");
+thumbnailElement.className;
+
+
+<script src="https://widgets.twimg.com/j/2/widget.js"></script>
+
+});
